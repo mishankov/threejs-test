a810192e21b81c8d87e937bc9122e2818333ff7b
mishankov <mishankov@mail.com>
2021-06-20 00:37:19 +0300

    base class

---
+++ src/BaseApplication.ts
@@ -0,0 +1,138 @@
+import { debug } from 'console';
+import * as THREE from 'three';
+import Stats from 'three/examples/jsm/libs/stats.module';
+
+export interface AnimatedObject {
+    object: THREE.Mesh;
+    animation: Function;
+}
+
+
+export function animate(app: BaseApplication) {
+    requestAnimationFrame(function() {
+        animate(app);
+    });
+
+    for (let index = 0; index < app.animatedObjects.length; index++) {
+        const element = app.animatedObjects[index];
+        
+        element.animation(element.object);
+    }
+
+    app.render();
+}
+
+export class BaseApplication {
+    scene: THREE.Scene;
+    camera: THREE.PerspectiveCamera;
+    renderer: THREE.WebGLRenderer;
+    stats: Stats;
+
+    animatedObjects: Array<AnimatedObject>;
+
+    DEBUG: boolean;
+
+    constructor(debug=false) {
+        this._init(debug);
+        this.init();
+    }
+
+    _init(debug = false) {
+        this.DEBUG = debug;
+        this.animatedObjects = new Array();
+
+        this.initScene();
+        this.initCamera();
+        this.initRenderer();
+        this.initLight();
+
+        window.addEventListener('resize', () => {
+            this.onResize();
+        }, false);
+
+        window.addEventListener('keypress', (event) => {
+            this.onKeypress(event.key);
+        });
+
+        this.initDebug();
+        
+    }
+
+    init() {}
+
+    initScene() {
+        this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color(0xffffff);
+    }
+
+    initCamera() {
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.camera.position.set(-25, 50, -25);
+        this.camera.lookAt(0, 0, 0);
+    }
+
+    initRenderer() {
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(this.renderer.domElement);
+    }
+
+    initLight() {
+        let dirLight = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+        dirLight.position.set(-100, 100, 100);
+        dirLight.target.position.set(0, 0, 0);
+        dirLight.castShadow = true;
+        dirLight.shadow.bias = -0.001;
+        dirLight.shadow.mapSize.width = 4096;
+        dirLight.shadow.mapSize.height = 4096;
+        dirLight.shadow.camera.near = 0.1;
+        dirLight.shadow.camera.far = 500.0;
+        dirLight.shadow.camera.near = 0.5;
+        dirLight.shadow.camera.far = 500.0;
+        dirLight.shadow.camera.left = 50;
+        dirLight.shadow.camera.right = -50;
+        dirLight.shadow.camera.top = 50;
+        dirLight.shadow.camera.bottom = -50;
+        this.scene.add(dirLight);
+    
+        let ambLight = new THREE.AmbientLight(0xFFFFFF, 0.25);
+        this.scene.add(ambLight);        
+    }
+
+    initDebug() {
+        if (this.DEBUG) {
+            this.scene.add(new THREE.AxesHelper(100));
+            
+            // FPS display
+            this.stats = Stats();
+            document.body.appendChild(this.stats.dom);
+        }
+    }
+
+    render() {
+        if (this.DEBUG) {
+            this.stats.begin();
+        }
+
+        this.renderer.render(this.scene, this.camera);
+        
+        if (this.DEBUG) {
+            this.stats.end();
+            this.stats.update();
+        }
+    }
+
+    onResize() {
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+
+        this.renderer.setSize(window.innerWidth, window.innerHeight);  
+    }
+
+    onKeypress(keyName: string) {}
+
+    logCamera() {
+        console.log('POSITION x: ' + this.camera.position.x + '; y: ' + this.camera.position.y + '; z: ' + this.camera.position.z);
+        console.log('ROTATION x: ' + THREE.MathUtils.radToDeg(this.camera.rotation.x) + '; y: ' + THREE.MathUtils.radToDeg(this.camera.rotation.y) + '; z: ' + THREE.MathUtils.radToDeg(this.camera.rotation.z));
+    }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,132 +1,48 @@
 import * as THREE from 'three';
-import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
-import Stats from 'three/examples/jsm/libs/stats.module';
+import * as BA from './BaseApplication'
 
 window.addEventListener('DOMContentLoaded', () => {
-    const app = new Application();
-    app.animatedCube();
-    animate(app);
+    const app = new Application(true);
+    BA.animate(app);
 });
 
-function animate(app: Application) {
-    requestAnimationFrame(function() {
-        animate(app);
-    });
 
-    app.render();
-    app.stats.update();
-}
-
-class Application {
-    scene: THREE.Scene;
-    camera: THREE.PerspectiveCamera;
-    renderer: THREE.WebGLRenderer;
-    controls: TrackballControls;
-    stats: Stats;
-
+class Application extends BA.BaseApplication {
     mainCube: SimpleCubeMesh;
-
-
-    constructor() {
-        this.init();
-    }
+    animatedCubes: Array<SimpleCubeMesh>;
 
     init() {
-        this.scene = new THREE.Scene();
-        this.scene.background = new THREE.Color(0xffffff);
-        this.scene.add(new THREE.AxesHelper(100));
-        
-        this.initCamera()
-    
-        this.renderer = new THREE.WebGLRenderer();
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(this.renderer.domElement);
-
-        this.initLight();
-
-        this.stats = Stats();
-        document.body.appendChild(this.stats.dom);
-
         this.addGround();
+        this.addAnimatedCubes();
         this.addMainCube();
-
-        window.addEventListener('resize', () => {
-            this.onResize();
-        }, false);
-
-        window.addEventListener('keypress', (event) => {
-            console.log(event.key + " pressed");
-            switch (event.key) {
-                case 'w':
-                    this.mainCube.position.x += Math.cos(this.mainCube.rotation.y);
-                    this.mainCube.position.z += -Math.sin(this.mainCube.rotation.y);
-                    break;
-                case 's':
-                    this.mainCube.position.x += -Math.cos(this.mainCube.rotation.y);
-                    this.mainCube.position.z += Math.sin(this.mainCube.rotation.y);
-                    break;
-                case 'a':
-                    const quaternion = new THREE.Quaternion();
-                    quaternion.setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), this.mainCube.rotation.y + THREE.MathUtils.degToRad(15));
-                    this.mainCube.quaternion.copy(quaternion);
-                    break;
-                case 'd':
-                    this.mainCube.rotateY(-15*Math.PI/180);
-                    break;
-                case '1':
-                    this.camera.lookAt(0, 0, 0);
-                    break;
-                case '0':
-                    this.camera.lookAt(this.camera.position);
-            }
-            this.mainCube.logState();
-        });
     }
 
-    initCamera() {
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        this.camera.position.set(-25, 50, -25);
-        this.camera.lookAt(0, 0, 0);
-    }
-
-    initLight() {
-        let dirLight = new THREE.DirectionalLight(0xFFFFFF, 1.0);
-        dirLight.position.set(-100, 100, 100);
-        dirLight.target.position.set(0, 0, 0);
-        dirLight.castShadow = true;
-        dirLight.shadow.bias = -0.001;
-        dirLight.shadow.mapSize.width = 4096;
-        dirLight.shadow.mapSize.height = 4096;
-        dirLight.shadow.camera.near = 0.1;
-        dirLight.shadow.camera.far = 500.0;
-        dirLight.shadow.camera.near = 0.5;
-        dirLight.shadow.camera.far = 500.0;
-        dirLight.shadow.camera.left = 50;
-        dirLight.shadow.camera.right = -50;
-        dirLight.shadow.camera.top = 50;
-        dirLight.shadow.camera.bottom = -50;
-        this.scene.add(dirLight);
-    
-        let ambLight = new THREE.AmbientLight(0xFFFFFF, 0.25);
-        this.scene.add(ambLight);        
-    }
-
-    render() {
-        this.stats.begin();
-        this.renderer.render(this.scene, this.camera);
-        this.stats.end();
-    }
-
-    onResize() {
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-        this.camera.updateProjectionMatrix();
-
-        this.renderer.setSize(window.innerWidth, window.innerHeight);  
-    }
-
-    logCamera() {
-        console.log(this.camera.position);
-        console.log('x: ' + THREE.MathUtils.radToDeg(this.camera.rotation.x) + '; y: ' + THREE.MathUtils.radToDeg(this.camera.rotation.y)+ '; z: ' + THREE.MathUtils.radToDeg(this.camera.rotation.z));
+    onKeypress(keyName: string) {
+        console.log(keyName+ " pressed");
+        switch (keyName) {
+            case 'w':
+                this.mainCube.position.x += Math.cos(this.mainCube.rotation.y);
+                this.mainCube.position.z += -Math.sin(this.mainCube.rotation.y);
+                break;
+            case 's':
+                this.mainCube.position.x += -Math.cos(this.mainCube.rotation.y);
+                this.mainCube.position.z += Math.sin(this.mainCube.rotation.y);
+                break;
+            case 'a':
+                const quaternion = new THREE.Quaternion();
+                quaternion.setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), this.mainCube.rotation.y + THREE.MathUtils.degToRad(15));
+                this.mainCube.quaternion.copy(quaternion);
+                break;
+            case 'd':
+                this.mainCube.rotateY(-15*Math.PI/180);
+                break;
+            case '1':
+                this.camera.lookAt(0, 0, 0);
+                break;
+            case '0':
+                this.camera.lookAt(this.camera.position);
+        }
+        this.mainCube.logState();
     }
 
     addGround() {
@@ -148,34 +64,44 @@ class Application {
         this.scene.add(this.mainCube);
     }
 
-    animatedCube() {
-        const cube = new SimpleCubeMesh(0x00ff00);
-        
-        this.scene.add(cube);
-        this.renderer.render(this.scene, this.camera);
-        
-        function animate(renderer: THREE.WebGLRenderer, scene: THREE.Scene, camera: THREE.Camera) {
-            requestAnimationFrame(function() {
-                animate(renderer, scene, camera);
-            });
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
-            cube.rotation.z += 0.01;
-            renderer.render(scene, camera);
+    addAnimatedCubes() {
+        this.animatedCubes = [
+            new SimpleCubeMesh(0x00ff00),
+            new SimpleCubeMesh(0xff0000, undefined, undefined, undefined, new THREE.Vector3(0, 10, 0)),
+            new SimpleCubeMesh(0x0000ff, undefined, undefined, undefined, new THREE.Vector3(0, 5, 0))
+        ];
+
+        for (let index = 0; index < this.animatedCubes.length; index++) {
+            const animatedCube = this.animatedCubes[index];
+            this.scene.add(animatedCube);
+
+            this.animatedObjects.push({
+                object: animatedCube,
+                animation: this.animateCube
+            })
         }
         
-        animate(this.renderer, this.scene, this.camera);
+    }
+
+    animateCube(cube: SimpleCubeMesh) {
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        cube.rotation.z += 0.01;
     }
 }
 
 class SimpleCubeMesh extends THREE.Mesh {
-    constructor(color=0x000000, width?: number, height?: number, depth?: number) {
+    constructor(color=0x000000, width?: number, height?: number, depth?: number, position?: THREE.Vector3) {
         const geometry = new THREE.BoxGeometry(width, height, depth);
         const material = new THREE.MeshStandardMaterial({color: color});
         super(geometry, material);
 
         this.castShadow = true;
         this.receiveShadow = true;
+
+        if (position !== undefined) {
+            this.position.set(position.x, position.y, position.z);
+        }
     }
 
     logState() {
